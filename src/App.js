563580ac16e8c61da8858dc6f906c90db411a033
Moataz Elmasry <motaz_1993@hotmail.com>
2022-05-05 18:06:34 +0400

    initial implementation of landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import "./App.css";
+import React, { useEffect, useState } from 'react';
 
-import { Typography } from "@material-ui/core";
-import React from "react";
+import { Layout, Drawer, Button, Menu, Row, Col } from 'antd';
+import 'antd/dist/antd.css';
 
 import Signer from "./components/signer";
 
+import "./App.css";
+
+
+const { Header, Content, Footer } = Layout;
+
+function AppHeader(props) {
+
+  const headerItems = [
+    { label: 'About' },
+    { label: 'FAQ' },
+    { label: <Button>Use App</Button> }
+  ]
+
+  return (
+    <Header {...props}>
+      <Row justify="end">
+        <Col span={6}>
+          <a href="/"><img className="logo-img" src="/white.png" alt="logo" /></a>
+        </Col>
+        <Col span={6} offset={8}>
+          <Menu mode="horizontal" className="header" items={headerItems} />
+        </Col>
+
+      </Row>
+    </Header >
+  )
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Typography variant="h4">
-          {" "}
-          Simple React App Using AlgoSigner{" "}
-        </Typography>
+    <Layout>
+      <AppHeader className="header" />
+      <Content style={{ padding: '0 50px' }}>
         <Signer />
-      </header>
-    </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Lotto Labs ©2021</Footer>
+    </Layout>
   );
 }
 
