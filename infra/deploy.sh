64aa0622ce9dd097ac37f286f38fa13fad210588
Moataz Elmasry <motaz_1993@hotmail.com>
2022-07-03 21:59:51 +0400

    added commant to deploy for when dryrun

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,6 +1,7 @@
 # Set wallet environment variables
 ONE=LHDAEQ7QDPK4CB56GPWNW5FQHW5N2B3D4PUP3E3MWI6OXGW5UH7WBZXTNI
 TWO=LZMV3V7XNQNN6T53DU6ENIGRWTE5DP5SYTSD6MTJ6RKEMK4IKX2XXONF3U
+THREE=PXSALUFZEU5DUB5N5FFHS7ZD4GTNI5CF7ULM4MEUVGA6ZPEIMFHN3O6XRU
 APP_ADDR=WCS6TVPJRBSARHLN2326LRU5BYVJZUKI2VJ53CAWKYYHDE455ZGKANWMGM
 
 # Deploy application to alogrand
@@ -17,10 +18,10 @@ goal app create \
 goal app optin --app-id 1 --from $TWO
 
 # Create purchase ticket transaction
-goal app call --app-id 1 --from $TWO --app-arg "str:purchase" --app-arg "int:1" -o purchase.tx
+goal app call --app-id 1 --from $TWO --app-arg "str:purchase" --app-arg "int:4" -o purchase.tx
 
 # Create payment transaction
-goal clerk send -a "1000000" -t $APP_ADDR -f $TWO -o purchase_payment.tx
+goal clerk send -a "4000000" -t $APP_ADDR -f $TWO -o purchase_payment.tx
 
 # Group transactions
 cat purchase.tx purchase_payment.tx > purchase_ticket.tx
@@ -33,4 +34,5 @@ goal clerk sign -i purchase_split-1.tx -o purchase-signed-1.tx
 
 # Re-combine signed
 cat purchase-signed-0.tx purchase-signed-1.tx > purchase-signed.tx
+# goal clerk dryrun --dryrun-dump -t purchase-signed.tx -o tx.dr
 goal clerk rawsend -f purchase-signed.tx
