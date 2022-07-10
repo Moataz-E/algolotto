d77a99fbb00e428760a2e8964b4ca77b7fc26b9d
Moataz Elmasry <motaz_1993@hotmail.com>
2022-07-10 16:42:55 +0400

    completed function that will show latest algorand block

--- src/components/dapp_panel.js
+++ src/components/dapp_panel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import algosdk from 'algosdk';
 import { Row, Col, Card, Button } from 'antd';
 import MyAlgoConnect from '@randlabs/myalgo-connect';
@@ -8,13 +8,19 @@ import TweenOne from 'rc-tween-one';
 
 import "./dapp_panel.css";
 
+const ALGOD_TOKEN = "";
+const ALGOD_SERVER = "https://node.testnet.algoexplorerapi.io";
+const ALGOD_PORT = "";
 
-function MyAlgoWallet() {
+const BLOCK_REFRESH_MS = 5000
+
+const algodClient = new algosdk.Algodv2(ALGOD_TOKEN, ALGOD_SERVER, ALGOD_PORT);
+
+function WalletConnect() {
   const myalgoSettings = {
     shouldSelectOneAccount: false,
     openManager: false
   };
-  const algodClient = new algosdk.Algodv2("", "https://node.testnet.algoexplorerapi.io", "");
   const myAlgoConnect = new MyAlgoConnect();
 
   const connect = async (e) => {
@@ -36,16 +42,44 @@ function MyAlgoWallet() {
     </Button>)
 }
 
+function BuyTicket() {
+  return <Button className="buy-button" shape="round" size="large" block>Buy</Button>
+}
+
+function AlgoInfo() {
+  const [latestBlock, setLatestBlock] = useState("");
+
+  async function getLatestBlock(e) {
+    try {
+      const status = await algodClient.status().do();
+      setLatestBlock(status["last-round"]);
+    } catch {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    getLatestBlock();
+    const interval = setInterval(() => {
+      getLatestBlock();
+    }, BLOCK_REFRESH_MS);
+    return () => clearInterval(interval);
+  }, [])
+
+  return <div><strong>Latest Algorand Block: </strong> {latestBlock}</div>
+}
+
 
 function DAppCard() {
   return (
-    <Card className="panel-card" title="Purchase Weekly Lottery" bordered={true} style={{ textAlign: 'center' }}>
+    <Card className="panel-card" title="Purchase Weekly Lottery" bordered={true} style={{ textAlign: 'left' }}>
       <div className="banner-card-body">
+        <AlgoInfo />
         <span className="description text-secondary">
           Display current round, draw date, tickets bought and box to purchase additional tickets
         </span>
-        <MyAlgoWallet />
-        <Button className="buy-button" shape="round" size="large" block>Buy</Button>
+        <WalletConnect />
+        <BuyTicket />
       </div>
     </Card>
   )
