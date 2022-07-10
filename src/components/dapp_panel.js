b802b5a1d5197b0e6c776b67f08cefd3ae095f16
Moataz Elmasry <motaz_1993@hotmail.com>
2022-07-10 13:51:47 +0400

    added myalgowallet

--- src/components/dapp_panel.js
+++ src/components/dapp_panel.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import algosdk from 'algosdk';
 import { Row, Col, Card, Button } from 'antd';
+import MyAlgoConnect from '@randlabs/myalgo-connect';
 
 import 'rc-texty/assets/index.css';
 import TweenOne from 'rc-tween-one';
 
-import "./dapp_panel.css"
+import "./dapp_panel.css";
+
+
+function MyAlgoWallet() {
+  const myalgoSettings = {
+    shouldSelectOneAccount: false,
+    openManager: false
+  };
+  const algodClient = new algosdk.Algodv2("", "https://node.testnet.algoexplorerapi.io", "");
+  const myAlgoConnect = new MyAlgoConnect();
+
+  const connect = async (e) => {
+    e.preventDefault()
+    const accounts = await myAlgoConnect.connect(myalgoSettings);
+    console.log(accounts);
+    // const sender = accounts[0].address;
+  };
+
+  return (
+    <Button
+      className="myalgo-connect-btn"
+      shape="round"
+      size="large"
+      block
+      onClick={connect}
+    >
+      Connect
+    </Button>)
+}
+
+
+function DAppCard() {
+  return (
+    <Card className="panel-card" title="Purchase Weekly Lottery" bordered={true} style={{ textAlign: 'center' }}>
+      <div className="banner-card-body">
+        <span className="description text-secondary">
+          Display current round, draw date, tickets bought and box to purchase additional tickets
+        </span>
+        <MyAlgoWallet />
+        <Button className="buy-button" shape="round" size="large" block>Buy</Button>
+      </div>
+    </Card>
+  )
+}
 
 
 export default function DAppPanel() {
@@ -12,14 +57,7 @@ export default function DAppPanel() {
     <Row justify="center" align="middle" className="dapp-panel-row">
       <Col lg={{ span: 8 }} md={{ span: 10 }} sm={{ span: 12 }} xs={{ span: 15 }} className="dapp-panel">
         <TweenOne animation={{ y: '-5rem' }}>
-          <Card className="panel-card" title="Purchase Weekly Lottery" bordered={true} style={{ textAlign: 'center' }}>
-            <div className="banner-card-body">
-              <span className="description text-secondary">
-                Display current round, draw date, tickets bought and box to purchase additional tickets
-              </span>
-              <Button className="buy-button" shape="round" size="large" block>Buy</Button>
-            </div>
-          </Card>
+          <DAppCard />
         </TweenOne>
       </Col>
     </Row>
